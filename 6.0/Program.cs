7a2b235dd07856dc24da0e29f9451c1cfdf0013b
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-03-26 10:18:33 +0500

    Add project files.

---
+++ 6.0/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 0;
+        bool validInput = false;
+
+        while (!validInput)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out number))
+            {
+                validInput = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, please try again.");
+            }
+        }
+
+      
+        for (int row = 1; row <= number; row++)
+        {
+         
+            for (int coloumn = 1; coloumn <= number; coloumn++)
+            {
+               
+                Console.Write("{0,4}", row * coloumn);
+            }
+            Console.WriteLine();
+        }
+    }
+}
